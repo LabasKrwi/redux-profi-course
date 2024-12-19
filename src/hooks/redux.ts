592c057a7455e967d55f5ecc7c93f7276a1ec693
unknown <ilukhankoperman@gmail.com>
2024-12-19 16:37:21 +0300

    0.2.0

---
+++ src/hooks/redux.ts
@@ -0,0 +1,7 @@
+import { TypedUseSelectorHook, useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "../store/store";
+import { useSelector } from "react-redux";
+
+
+const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
